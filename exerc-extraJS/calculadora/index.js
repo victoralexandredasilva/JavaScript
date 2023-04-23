c677eb5ef2efea59789cab1f3f81a67a45daa8c4
victoralexandredasilva <124846412+victoralexandredasilva@users.noreply.github.com>
2023-04-22 23:42:15 -0300

    calculadora em Javacript

---
+++ exerc-extraJS/calculadora/index.js
@@ -0,0 +1,28 @@
+const screen = document.querySelector('.screen')
+
+
+function insert(num) {
+    let numero = screen.innerHTML
+    screen.innerHTML = numero + num
+    if(numero.length > 15) {
+        window.alert('erro')
+    }
+}
+
+function clean() {
+    screen.innerHTML = ''
+}
+
+function backString() {
+    let resultado = screen.innerHTML
+    screen.innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calcular() {
+    let resultado = screen.innerHTML
+    if(resultado) {
+        screen.innerHTML = eval(resultado)
+    } else {
+        screen.innerHTML = '0'
+    }
+}
